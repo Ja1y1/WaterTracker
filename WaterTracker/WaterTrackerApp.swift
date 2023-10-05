d3ccc6baf1d12c0558d9d49d4e06b3cef73ec120
Ja1y1 <146494173+Ja1y1@users.noreply.github.com>
2023-10-05 15:11:45 +0300

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by JAY on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
